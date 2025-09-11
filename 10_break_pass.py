a02ebb7a699652284542cd4895510f95e4dbc2dd
KJY <KJY@naver.com>
2025-09-11 11:19:51 +0900

    continue 문 추가

--- 10_break_pass.py
+++ 10_break_pass.py
@@ -1,11 +1,28 @@
 cup = 0
 running = True
 
-while True:
-    cup += 1
-    print(cup)
-    if cup == 10:
-        #running = False
-        break
+# while True:
+#     cup += 1
+#     print(cup)
+#     if cup == 10:
+#         #running = False
+#         break
 
-print('while 문 종료')
+# print('while 문 종료')
+
+for i in range(1,10):
+
+    if i == 3 | i == 6 | i == 9:
+
+    # if i % 3 == 0:
+
+    # if i == 3:
+    #     continue
+    # if i == 6:
+    #     continue
+    # if i == 9:
+    #     continue
+
+        continue
+
+    print(i)
