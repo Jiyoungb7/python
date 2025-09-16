4451beb4619be15f0f76f4098b94d63d5c402f50
KJY <KJY@naver.com>
2025-09-16 16:39:12 +0900

    test

---
+++ advance/13_test.py
@@ -0,0 +1,21 @@
+
+text = input('값을 넣으면 숫자로 변환 됩니다.')
+
+try:
+    num = int(text)
+except Exception:
+    print('숫자를 입력해주세요.')
+    text = input('값을 넣으면 숫자로 변환 됩니다.')
+finally:
+    num = int(text)
+    print(f'당신이 입력한 값 :{num} 이 숫자로 변환 되었습니다.')
+
+# while True:
+#     text = input('값을 넣으면 숫자로 변환 됩니다.')
+#     try:
+#         num = int(text)  # 숫자로 변환 시도
+#         break  # 성공하면 반복문 탈출
+#     except ValueError:  # 숫자가 아니면 에러 발생
+#         print('숫자를 입력해주세요.')
+#
+# print(f'당신이 입력한 값 : {num} 이 숫자로 변환 되었습니다.')
